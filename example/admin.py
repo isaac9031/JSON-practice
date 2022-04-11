00be1fa15dbbc10144984de139444770bd6c4a0a
Curtis Schlak <>
2022-04-11 12:17:14 -0500

    add admin to the thing.

--- example/admin.py
+++ example/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog, Comment
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
